7b4dcc760117cc203ee84f18c876096fa33e39b5
MiguelAS2104 <ms1941400@gmail.com>
2026-05-05 14:22:03 -0600

    componente datos personales

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,37 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  const datosPersonales = {
+    nombre: "Miguel Angel",
+    edad: 20,
+    ciudad: "Juigalpa",
+    profesion: "Ingeniero de sistemas",
+    correo: "ms1941400@email.com",
+  };
+
   return (
-    <>
-      <h1>Componente principal</h1>
-      <h2>Subtitulo</h2>
-    </>
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Mis Datos Personales</h1>
+
+      <ul>
+        <li>
+          <strong>Nombre:</strong> {datosPersonales.nombre}
+        </li>
+        <li>
+          <strong>Edad:</strong> {datosPersonales.edad}
+        </li>
+        <li>
+          <strong>Ciudad:</strong> {datosPersonales.ciudad}
+        </li>
+        <li>
+          <strong>Profesión:</strong> {datosPersonales.profesion}
+        </li>
+        <li>
+          <strong>Correo:</strong> {datosPersonales.correo}
+        </li>
+      </ul>
+    </div>
   );
-}
+};
+
 export default App;
