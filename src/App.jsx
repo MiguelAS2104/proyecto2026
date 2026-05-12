a6f434c2fab3ecb85daf16afe491f63573c67278
MiguelAS2104 <ms1941400@gmail.com>
2026-05-12 13:59:18 -0600

    Cambios documento 8 de mayo

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,34 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Encabezado from "./components/navegacion/Encabezado";
+
+import Inicio from "./views/Inicio"
+import Categorias from "./views/Categorias"
+import Catalogo from "./views/Catalogo"
+import Productos from "./views/Productos"
+import Login from "./views/Login"
+import RutaProtegida from "./components/rutas/RutaProtegida"
+import Pagina404 from "./views/Pagina404"
+
 import './App.css'
 
 const App = () => {
-  const datosPersonales = {
-    nombre: "Miguel Angel",
-    edad: 20,
-    ciudad: "Juigalpa",
-    profesion: "Ingeniero de sistemas",
-    correo: "ms1941400@email.com",
-  };
-
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>Mis Datos Personales</h1>
-
-      <ul>
-        <li>
-          <strong>Nombre:</strong> {datosPersonales.nombre}
-        </li>
-        <li>
-          <strong>Edad:</strong> {datosPersonales.edad}
-        </li>
-        <li>
-          <strong>Ciudad:</strong> {datosPersonales.ciudad}
-        </li>
-        <li>
-          <strong>Profesión:</strong> {datosPersonales.profesion}
-        </li>
-        <li>
-          <strong>Correo:</strong> {datosPersonales.correo}
-        </li>
-      </ul>
-    </div>
+    <Router>
+      <Encabezado />
+      <main className="margen-superior-main">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<RutaProtegida><Inicio /></RutaProtegida>} />
+          <Route path="/categorias" element={<RutaProtegida><Categorias /></RutaProtegida>} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/productos" element={<RutaProtegida><Productos /></RutaProtegida>} />
+          <Route path="*" element={<Pagina404 />} />
+        </Routes>
+      </main>
+    </Router>
   );
-};
+}
 
 export default App;
